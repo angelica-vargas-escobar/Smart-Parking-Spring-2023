dde0a97e17c62bcddab304fe9101b1e7b48e83e9
angelica-vargas-escobar <70992079+angelica-vargas-escobar@users.noreply.github.com>
2023-04-29 18:41:33 -0700

    Update ParkingLot.cs

--- SP/SmartParking/ParkingLot.cs
+++ SP/SmartParking/ParkingLot.cs
@@ -21,11 +21,11 @@ namespace TeamVaxxers
     {
         FirebaseClient client = new FirebaseClient("https://heymotocarro-1a1d4.firebaseio.com/");
         Graphics G;
-        Rectangle[] rect= new Rectangle[6];
+        Rectangle[] rect = new Rectangle[6];
         public ParkingLot()
         {
             InitializeComponent();
-           // WindowState = FormWindowState.Maximized;
+            // WindowState = FormWindowState.Maximized;
         }
         private void ParkingLot_Load(object sender, EventArgs e)
         {
@@ -34,36 +34,111 @@ namespace TeamVaxxers
         private void loadData(object sender, EventArgs e)
         {
             getBeaconDataAsync();
+            // DrawSlots();
         }
         private void DrawSlots(object sender, EventArgs e)
+        //private async void DrawSlots()
         {
-            Pen blackPen = new Pen(Color.Black, 0);
-            SolidBrush myBrush = new SolidBrush(Color.SkyBlue);
-            SolidBrush myBrush2 = new SolidBrush(Color.YellowGreen);
+            /*                int startX1 = 50;
+                            int startY1 = 50;
+                            // int distance = 50;
 
+                            Pen blackPen = new Pen(Color.Thistle, 10);
+                            SolidBrush myBrush = new SolidBrush(Color.GreenYellow);
+                            SolidBrush myBrush2 = new SolidBrush(Color.Red);*/
 
-            // Create rectangle and Draw rectangle to screen.
-            for (int i = 0; i < 6; i++)
+            // hard coded drawing rectangles
+            int startX1 = 50;
+            int startY1 = 50;
+            // int distance = 50;
+
+            // Pen blackPen = new Pen(Color.Black, 0);
+            Pen blackPen = new Pen(Color.Thistle, 10);
+            //Pen blackPen2 = new Pen(Color.Thistle, 10);
+            SolidBrush myBrush = new SolidBrush(Color.GreenYellow);
+            SolidBrush myBrush2 = new SolidBrush(Color.Red);
+
+            for (int i = 0; i < 3; i++)
             {
-               rect[i] = new Rectangle(100 * i, 100, 100, 200);
-                G.FillRectangle(myBrush, rect[i]);
-                G.DrawRectangle(blackPen, rect[i]);
+                if (i != 6) // find out how to fill specific slot
+                {
+                    rect[i] = new Rectangle(startX1 + 100 * i, startY1, 100, 200);
+                    G.FillRectangle(myBrush, rect[i]);
+                    G.DrawRectangle(blackPen, rect[i]);
+
+                    DrawStringFloatFormat((i + 1).ToString(), startX1 + 100 * i + 50, startY1 + 100);
+                }
+                else
+                {
+                    rect[i] = new Rectangle(startX1 + 100 * i, startY1, 100, 200);
+                    G.FillRectangle(myBrush2, rect[i]);
+                    G.DrawRectangle(blackPen, rect[i]);
+
+                    DrawStringFloatFormat((i + 1).ToString(), startX1 + 100 * i + 50, startY1 + 100);
+                }
             }
 
-            //Update parking space rectangle color
-            G.FillRectangle(myBrush2, rect[2]);
-            G.FillRectangle(myBrush2, rect[5]);
+            startY1 += 200;
 
-            //draw parking numbers
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 3; i++)
             {
-                 DrawStringFloatFormat((i + 1).ToString(), 100 * i + 50, 200.0F);
+                if ((i + 3 != 5) && (i + 3 != 3)) // find out how to fill specific slots
+                {
+                    rect[i + 3] = new Rectangle(startX1 + 100 * i, startY1, 100, 200);
+                    G.FillRectangle(myBrush, rect[i + 3]);
+                    G.DrawRectangle(blackPen, rect[i + 3]);
+
+                    DrawStringFloatFormat((i + 4).ToString(), startX1 + 100 * i + 50, startY1 + 100);
+                }
+                else
+                {
+                    rect[i + 3] = new Rectangle(startX1 + 100 * i, startY1, 100, 200);
+                    G.FillRectangle(myBrush2, rect[i + 3]);
+                    G.DrawRectangle(blackPen, rect[i + 3]);
+
+                    DrawStringFloatFormat((i + 4).ToString(), startX1 + 100 * i + 50, startY1 + 100);
+                }
             }
         }
+
+        /*
+        G.FillRectangle(myBrush, rect[j + 2]);
+        G.FillRectangle(myBrush2, rect[j + 2]);
+        G.FillRectangle(myBrush, rect[j + 5]);
+        G.FillRectangle(myBrush2, rect[j + 5]);
+        */
+
+
+        // original code
+        /*
+        Pen blackPen = new Pen(Color.Black, 0);
+        SolidBrush myBrush = new SolidBrush(Color.GreenYellow);
+        SolidBrush myBrush2 = new SolidBrush(Color.Red);
+
+
+        // Create rectangle and Draw rectangle to screen.
+        for (int i = 0; i < 6; i++)
+        {
+           rect[i] = new Rectangle(100 * i, 100, 100, 200);
+            G.FillRectangle(myBrush, rect[i]);
+            G.DrawRectangle(blackPen, rect[i]);
+        }
+
+        //Update parking space rectangle color
+        G.FillRectangle(myBrush2, rect[3]);
+        G.FillRectangle(myBrush2, rect[5]);
+
+        //draw parking numbers
+        for (int i = 0; i < 6; i++)
+        {
+             DrawStringFloatFormat((i + 1).ToString(), 100 * i + 50, 200.0F);
+        }
+        */
+
         private async void getBeaconDataAsync() // grabs population from database 
         {
-            
-    
+
+
             //******************** Get initial list of beacons ***********************//
             var BeaconsSet = await client
                .Child("Beacons/")//Prospect list
@@ -75,7 +150,7 @@ namespace TeamVaxxers
 
 
         }
-
+ 
         private void onChildChanged() // Waits for data base to start with variable
         {
 
@@ -85,19 +160,47 @@ namespace TeamVaxxers
             var subscription = observable
                 .Subscribe(x =>
                 {
-                    Console.WriteLine($"beacon id: { x.Object.Id} [{ x.Object.D1}]");                 
+                    Console.WriteLine($"beacon id: { x.Object.Id} [{ x.Object.D1}]");
                 });
-           
+
         }
+
+        private async void getParkingMapDataAsync() // grabs population from database 
+        {
+            var parkingMapData = await client
+               .Child("ParkingMap/")//Prospect list
+               .OnceSingleAsync<ParkingMap>();
+
+            onParkingMapChanged();
+
+        }
+        private void onParkingMapChanged() // Waits for data base to start with variable
+        {
+            var child = client.Child("ParkingMap/data");
+            var observable = child.AsObservable<ParkingMap>();
+            var subscription = observable.Subscribe(x =>
+                {
+                    Console.WriteLine($"Parking Map data received");
+
+                    Console.WriteLine($"    X: {x.Object.Position[0].X}, Y: { x.Object.Position[0].Y}");
+                    Console.WriteLine($"    X: {x.Object.Position[1].X}, Y: { x.Object.Position[1].Y}");
+                    Console.WriteLine($"    X: {x.Object.Position[2].X}, Y: { x.Object.Position[2].Y}");
+                    Console.WriteLine($"    X: {x.Object.Position[3].X}, Y: { x.Object.Position[3].Y}");
+
+                    Console.WriteLine();
+                });
+
+        }
+
         public void DrawStringFloatFormat(String drawString, float x, float y)
         {
             // Create font and brush.
             Font drawFont = new Font("Arial", 16);
-            SolidBrush drawBrush = new SolidBrush(Color.Black);          
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
 
             // Set format of string.
             StringFormat drawFormat = new StringFormat();
-           // drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            // drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
 
             // Draw string to screen.
             G.DrawString(drawString, drawFont, drawBrush, x, y, drawFormat);
@@ -111,5 +214,32 @@ namespace TeamVaxxers
             }
 
         }
+
+        /*        private void button2_Click(object sender, EventArgs e)
+                {
+                    InitializeComponent();
+                    getBeaconDataAsync();
+                    getParkingMapDataAsync();
+                    onParkingMapChanged();
+                    // getSensorAsync();
+                }*/
+
+        /*        private void button4_Click(object sender, EventArgs e)
+                {
+                    InitializeComponent();
+                    getBeaconDataAsync();
+                    getParkingMapDataAsync();
+                }
+
+                private void button3_Click(object sender, EventArgs e)
+                {
+
+                }
+
+                private void button2_Click_1(object sender, EventArgs e)
+                {
+
+                }*/
     }
 }
+
