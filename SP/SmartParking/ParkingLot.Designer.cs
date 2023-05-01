663762dbe35087300c22421c204ab9048f62d0f0
angelica-vargas-escobar <70992079+angelica-vargas-escobar@users.noreply.github.com>
2023-05-01 15:36:30 -0700

    Update ParkingLot.Designer.cs

--- SP/SmartParking/ParkingLot.Designer.cs
+++ SP/SmartParking/ParkingLot.Designer.cs
@@ -30,15 +30,12 @@ namespace TeamVaxxers
         {
             this.loadBtn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // loadBtn
             // 
             this.loadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.loadBtn.Location = new System.Drawing.Point(16, 0);
+            this.loadBtn.Location = new System.Drawing.Point(16, 13);
             this.loadBtn.Margin = new System.Windows.Forms.Padding(4);
             this.loadBtn.Name = "loadBtn";
             this.loadBtn.Size = new System.Drawing.Size(100, 28);
@@ -50,39 +47,21 @@ namespace TeamVaxxers
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(124, 0);
+            this.button1.Location = new System.Drawing.Point(138, 13);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 2;
-            this.button1.Text = "DrawSquare";
+            this.button1.Text = "Parking Lot";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.DrawSlots);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(0, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 1;
-            // 
             // 
             // ParkingLot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1067, 805);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.loadBtn);
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -97,8 +76,5 @@ namespace TeamVaxxers
 
         private System.Windows.Forms.Button loadBtn;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
